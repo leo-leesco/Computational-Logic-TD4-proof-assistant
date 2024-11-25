4a19ac160a50936fbb036e1ca96dc24639839737
Léo LEESCO <leo.leesco@gmail.com>
2024-11-25 17:16:18 +0100

    added parser test

--- simple/prover.ml
+++ simple/prover.ml
@@ -151,3 +151,46 @@ let () =
   in
   print_endline (string_of_tm fals);
   print_endline (string_of_ty (infer_type [] fals))
+
+let () =
+  let l =
+    [
+      "A => B";
+      (*"A ⇒ B";*)
+      "A /\\ B";
+      (*"A ∧ B";*)
+      "T";
+      "A \\/ B";
+      (*"A ∨ B";*)
+      "_";
+      "not A";
+      (*"¬ A";*)
+    ]
+  in
+  List.iter
+    (fun s ->
+      Printf.printf "the parsing of %S is %s\n%!" s
+        (string_of_ty (ty_of_string s)))
+    l
+
+let () =
+  let l =
+    [
+      "t u v";
+      "fun (x : A) -> t";
+      (*"λ (x : A) → t";*)
+      "(t , u)";
+      "fst(t)";
+      "snd(t)";
+      "()";
+      "case t of x -> u | y -> v";
+      "left(t,B)";
+      "right(A,t)";
+      "absurd(t,A)";
+    ]
+  in
+  List.iter
+    (fun s ->
+      Printf.printf "the parsing of %S is %s\n%!" s
+        (string_of_tm (tm_of_string s)))
+    l

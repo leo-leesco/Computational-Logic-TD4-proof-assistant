9a9eae5731cb734e98fc953e8dc8b7a75ef55fea
Léo LEESCO <leo.leesco@gmail.com>
2025-06-27 17:02:50 -0700

    pair intro elimination

---
+++ simple/README.md
@@ -0,0 +1,56 @@
+# Simply typed theorem prover
+
+## Usage
+
+Run `dune exec ./proving.exe`. This will prompt you for a proposition you would like to prove and to input a series of tactics to prove it. This results in a proof term written as a $\lambda$-term.
+
+The prover will summarize the commands inputted at the end of the proof, which can then be loaded again (as is) :
+```shell
+cat <proof> | dune exec ./proving.exe
+```
+
+## Tactics
+
+### `exact`
+
+Requires an element of the context whose type matches the current goal.
+
+This is the *axiom* rule.
+
+### `intro`
+
+In presence of a connector, we can prove it as the result of an introduction rule (which "creates" it).
+
+#### `intro x`
+
+In case the goal is an arrow, introduces a new identifier which matches its domain, leaving the codomain to be proven from the context.
+
+This is the *arrow introduction* rule.
+
+#### `intro`
+
+In case the goal is a conjunction type, divides the proof into two subproofs of both types of the pair.
+
+This is the *conjunction introduction* rule.
+
+### `elim`
+
+Tries to prove the goal by using connectors that could have led to the current goal (basically backtracking).
+
+#### `elim f`
+
+Provide an identifier whose type is an arrow whose codomain matches the current goal, leaving the domain to be proven.
+
+This is the *arrow elimination* rule.
+
+#### `cut A`
+
+Divides the proof into two subproofs, one of the lemma `A`, the other that `A` is indeed sufficient to prove the current goal.
+
+This is the *full arrow elimination* rule.
+
+#### `fst p` and `snd p`
+
+Provide an identifier whose type is a conjunction type and either its first or second component matches the current goal.
+
+This are the *left* and *right elimination* rules.
--- simple/proving.ml
+++ simple/proving.ml
@@ -1,6 +1,8 @@
 open Prover
 open Expr
 
+let commands = ref []
+
 let rec prove ctx formula =
   print_endline (string_of_sequent (ctx, formula));
   print_string "? ";
@@ -11,6 +13,7 @@ let rec prove ctx formula =
   in
   let cmd, arg =
     let cmd = input_line stdin in
+    commands := cmd :: !commands;
     let n = try String.index cmd ' ' with Not_found -> String.length cmd in
     let c = String.sub cmd 0 n in
     let a = String.sub cmd n (String.length cmd - n) in
@@ -26,6 +29,7 @@ let rec prove ctx formula =
             let x = arg in
             let t = prove ((x, a) :: ctx) b in
             Fn (x, a, t)
+      | And (a, b) -> Pair (prove ctx a, prove ctx b)
       | _ -> error "Don't know how to introduce this.")
   | "exact" ->
       let t = tm_of_string arg in
@@ -42,11 +46,29 @@ let rec prove ctx formula =
               let u = prove ctx a in
               App (t, u)
         | _ -> error "Don't know how to eliminate this.")
+  | "cut" ->
+      if arg = "" then error "Please provide an argument for cut."
+      else
+        let subgoal = ty_of_string arg in
+        App (prove ctx (Imp (subgoal, formula)), prove ctx subgoal)
+  | "fst" -> (
+      if arg = "" then error "Please provide an argument for cut."
+      else
+        match List.assoc arg ctx with
+        | And (a, _) when a = formula -> Fst (Var arg)
+        | _ -> error "This identifier is not a conjunction in the context.")
+  | "snd" -> (
+      if arg = "" then error "Please provide an argument for cut."
+      else
+        match List.assoc arg ctx with
+        | And (_, b) when b = formula -> Snd (Var arg)
+        | _ -> error "This identifier is not a conjunction in the context.")
   | cmd -> error ("Unknown command: " ^ cmd)
 
 let () =
   print_endline "Please enter the formula to prove:";
   let a = input_line stdin in
+  commands := a :: !commands;
   let a = ty_of_string a in
   print_endline "Let's prove it.";
   let t = prove [] a in
@@ -56,4 +78,6 @@ let () =
   print_string "Typechecking... ";
   flush_all ();
   assert (infer_type t = a);
-  print_endline "ok."
+  print_endline "ok.";
+  print_endline "Commands :";
+  print_endline (String.concat "\n" (List.rev !commands))

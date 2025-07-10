ddea8369975c6a346e54342f0415114b4cc6d319
Léo LEESCO <leo.leesco@gmail.com>
2025-07-10 16:48:38 -0300

    alpha eq

--- dependent/prover.ml
+++ dependent/prover.ml
@@ -26,6 +26,20 @@ let rec subst x u = function
         let y' = fresh_var () in
         subst x u (Pi (y', subst y (Var y') a, subst y (Var y') t))
 
+let rec alpha t u =
+  if debug then print_endline ("𝝰:" ^ to_string t ^ " <> " ^ to_string u);
+  match (t, u) with
+  | Var x, Var y -> x = y
+  | Abs (x, a, t), Abs (y, b, u) | Pi (x, a, t), Pi (y, b, u) ->
+      if x = y then alpha a b && alpha t u
+      else
+        let x' = fresh_var () in
+        alpha (subst x (Var x') a) (subst y (Var x') b)
+        && alpha (subst x (Var x') t) (subst y (Var x') u)
+  | App (t, u), App (t', u') -> alpha t t' && alpha u u'
+  | Type, Type -> true
+  | _, _ -> false
+
 let%test_unit "subst" =
   [%test_eq: expr] (subst "x" (Var "y") (Var "x")) (Var "y");
   [%test_eq: expr] (subst "x" (Var "y") (Var "z")) (Var "z");
@@ -37,6 +51,14 @@ let%test_unit "subst" =
     (subst "x" (Var "y") (Abs ("x", Var "A", Var "x")))
     (Abs ("x1", Var "A", Var "x1"))
 
+let%test "var1 𝝰-equiv" = alpha (Var "x") (Var "x")
+let%test "var2 𝝰-equiv" = not (alpha (Var "x") (Var "y"))
 
+let%test "abs 𝝰-equiv" =
+  alpha (Abs ("y", Var "A", Var "y")) (Abs ("x", Var "A", Var "x"))
 
+let%test "subst 𝝰-equiv" =
+  alpha
+    (subst "x" (Var "y") (Abs ("x", Var "A", Var "x")))
+    (Abs ("x", Var "A", Var "x"))
 
